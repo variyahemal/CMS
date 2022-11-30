98cd198c810c833ac80713647de96d99f7047cf2
Hemal Variya <34329437+variyahemal@users.noreply.github.com>
2022-11-30 23:56:49 +0530

    update

---
+++ CMS.Repository/Implementation/GenericRepository.cs
@@ -0,0 +1,66 @@
+﻿using CMS.Repository.Interface;
+using Microsoft.EntityFrameworkCore.Query;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CMS.Repository.Implementation
+{
+    public class GenericRepository<T> : IGenericRepository<T> where T : class
+    {
+        private readonly ApplicationDbContext _context;
+        public void Add(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<T> AddAsync(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<T> DeleteAsync(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteRange(List<T> entityList)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<T> GetAll(Expression<Func<T, bool>> filter = null, Func<IQueryable<T>, IOrderedQueryable<T>> orderBy = null, Func<IQueryable<T>, IIncludableQueryable<T, object>> include = null, bool disabledTracking = true)
+        {
+            throw new NotImplementedException();
+        }
+
+        public T GetById(object id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public T GetByIdAsync(Expression<Func<T, bool>> filter = null, Func<IQueryable<T>, IIncludableQueryable<T, object>> include = null, bool disabledTracking = true)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(T entity)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<T> UpdateAsync(T entity)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
--- CMS.Repository/Interface/IGenericRepository.cs
+++ CMS.Repository/Interface/IGenericRepository.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore.Query;
+﻿using Microsoft.EntityFrameworkCore.Diagnostics;
+using Microsoft.EntityFrameworkCore.Query;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -16,5 +17,18 @@ namespace CMS.Repository.Interface
           Func<IQueryable<T>, IOrderedQueryable<T>> orderBy = null,
           Func<IQueryable<T>, IIncludableQueryable<T, object>> include = null,
           bool disabledTracking = true);
+
+        T GetById(object id);
+        T GetByIdAsync(Expression<Func<T, bool>> filter = null,
+            Func<IQueryable<T>, IIncludableQueryable<T, object>> include = null,
+            bool disabledTracking = true);
+        void Add(T entity);
+        Task<T> AddAsync(T entity);
+        void Update(T entity);
+        Task<T> UpdateAsync(T entity);
+
+        void Delete(T entity);
+        Task<T> DeleteAsync(T entity);
+        void DeleteRange(List<T> entityList);
     }
 }
