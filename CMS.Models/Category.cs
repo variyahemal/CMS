29d3f7b70ee5ae05095c8424b5ff0bd4fc0a47bb
Hemal Variya <34329437+variyahemal@users.noreply.github.com>
2022-11-30 23:42:36 +0530

    Add project files.

---
+++ CMS.Models/Category.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CMS.Models
+{
+    public class Category
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        [DataType(DataType.Date)]
+        public DateTime CreatedDate { get; set; }
+    }
+}
---
+++ CMS.Repository/Interface/IGenericRepository.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore.Query;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CMS.Repository.Interface
+{
+    public interface IGenericRepository<T>
+    {
+        IEnumerable<T> GetAll(
+          Expression<Func<T, bool>> filter = null,
+
+          Func<IQueryable<T>, IOrderedQueryable<T>> orderBy = null,
+          Func<IQueryable<T>, IIncludableQueryable<T, object>> include = null,
+          bool disabledTracking = true);
+    }
+}
